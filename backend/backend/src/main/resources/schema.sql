d12de75bd05e10a82abae0037ab744342a69941f
FakeKorra? <abramovaleva3007@gmail.com>
2025-06-13 07:16:28 +0300

    Delete backend/backend directory

--- backend/backend/src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS students (
-    id SERIAL PRIMARY KEY,
-    full_name VARCHAR(100) NOT NULL,
-    group_name VARCHAR(50) NOT NULL,
-    enrollment_date TIMESTAMP WITH TIME ZONE NOT NULL
-);
